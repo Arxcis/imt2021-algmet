ea911d1add9dce7bf62c52fc1acc45e219bb4795
Jonas J. Solsvik <jonas.solsvik@gmail.com>
2017-08-31 17:42:06 +0200

    oppg 1 hefte
    Parsing the postfix expression into a
    parsing tree. Next up is to traverse the parse tree
    in a preorder manner, and postorder manner.

---
+++ hefte1/main.cpp
@@ -0,0 +1,76 @@
+
+#include <iostream>
+#include <cstring>
+#include <cassert>
+
+struct Node {
+    char  key = 0;
+    Node* next  = this; // For the stack
+    Node* left  = this; // For the tree 
+    Node* right = this; // For the tree 
+};
+
+struct Stack {
+    Node* head = new Node{};
+    Node* foot = new Node{};
+    Stack() { head->next = foot; }
+};
+
+
+void display(Stack* stack) {
+    Node* it = stack->head->next;
+    std::cout << "Stack ||";
+
+    while (it != it->next) {
+        std::cout << it->key;
+        it = it->next;
+    }
+    std::cout << "\n";
+}
+
+void push(Stack* stack, Node* node) {
+    node->next = stack->head->next;
+    stack->head->next = node;
+}
+
+Node* pop(Stack* stack) {
+    Node* node = stack->head->next;
+    stack->head->next = node->next;
+    return node;
+}
+
+
+Node* buildTree(const char* postfixstring) {
+    Stack* const stack = new Stack{};
+
+
+    for (size_t i = 0; i < strlen(postfixstring); ++i) {
+        char c = postfixstring[i];
+
+        if (c == '*' || c == '+') {
+            Node* node = new Node{};
+            node->key   = c;
+            node->left  = pop(stack);
+            node->right = pop(stack);
+            push(stack, node);
+        }
+        else if ( c >= '0' && c <= '9') {
+            Node* node = new Node{};
+            node->key = c;
+            push(stack, node);
+        }
+        else {
+            std::cout << "Invalid character...\n";
+            assert(1);
+        }
+        display(stack);
+    }
+
+    return stack->head->next;
+}
+
+int main () {    
+    Node* root = buildTree("598+46**7+*");
+    std::cout << "hehe\n";  
+    return 0; 
+}
---
+++ hefte1/run.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+g++ -g -std=c++14 main.cpp -o ../build/main
+./../build/main
